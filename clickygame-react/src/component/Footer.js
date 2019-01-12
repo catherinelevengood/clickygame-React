11c7a6b6a48a237de41811126f22927d12c4c3b0
Catherine Levengood <angel_avelanes@yahoo.com>
2019-01-11 21:59:30 -0500

    add files

---
+++ clickygame-react/src/component/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Footer = () => (
+
+    <footer className="footer bg-info">
+        <div className="container">
+            <span className="text-dark">Copyright 2018
+            </span>
+        </div>
+    </footer>
+)
