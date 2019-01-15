2d27abdaa1cd47341a685f8092b3a6a8f8a09cf1
Catherine Levengood <angel_avelanes@yahoo.com>
2019-01-14 19:14:10 -0500

    added files

---
+++ clickygame-react/src/component/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "React";
+import "./Footer.css";
+ 
+// set the footer to a copyright string 
+const Footer = props => (
+    <div className="footer">
+        <p class="text-muted text-center">Images Featuring <i>Disney Princess </i> character are the property of their respective owners.</p>
+     
+    </div>
+);
+
+export default Footer;
